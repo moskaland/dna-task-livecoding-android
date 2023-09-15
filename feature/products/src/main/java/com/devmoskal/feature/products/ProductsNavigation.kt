df66e341591e9e4c27e50fac6cbbca6e71160535
Moskal <andrzej.mosk@gmail.com>
2023-09-15 17:22:45 +0200

    fix collectAsState - use collectAsStateWithLifecycle instead

--- feature/products/src/main/java/com/devmoskal/feature/products/ProductsNavigation.kt
+++ feature/products/src/main/java/com/devmoskal/feature/products/ProductsNavigation.kt
@@ -1,7 +1,8 @@
 package com.devmoskal.feature.products
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 
@@ -17,9 +18,12 @@ fun NavGraphBuilder.productsScreen(
 
 @Composable
 fun ProductsRoute(productsViewModel: ProductsViewModel) {
+    val products by productsViewModel.products.collectAsStateWithLifecycle()
+    val cart by productsViewModel.cart.collectAsStateWithLifecycle()
+
     ProductsScreen(
-        productsViewModel.products.collectAsState().value,
-        productsViewModel.cart.collectAsState().value,
+        products,
+        cart,
         productsViewModel::getProducts,
         productsViewModel::addToCart,
         productsViewModel::removeFromCart

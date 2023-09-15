6859dbba49c71633c6ec9f5220edba0d42b41e03
Moskal <andrzej.mosk@gmail.com>
2023-09-15 15:36:23 +0200

    extract card module

---
+++ core/service/src/main/java/com/devmoskal/core/service/cardReader/mock/MockCardReaderService.kt
@@ -0,0 +1,24 @@
+package com.devmoskal.core.service.cardReader.mock
+
+import com.devmoskal.core.model.CardData
+import com.devmoskal.core.service.cardReader.CardReaderService
+import com.devmoskal.core.service.cardReader.model.CardReaderException
+import kotlinx.coroutines.delay
+import java.util.Calendar
+import java.util.UUID
+
+class MockCardReaderService : CardReaderService {
+
+    override suspend fun readCard(): CardData {
+        val second = Calendar.getInstance().get(Calendar.SECOND)
+
+        if (second <= 5) {
+            // User will need some time to use the card
+            delay(4000)
+            return CardData(UUID.randomUUID().toString())
+        }
+
+        throw CardReaderException()
+    }
+
+}

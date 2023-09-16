f7f7ce0f14b145ca7f13cc8497694eba617d51de
Moskal <andrzej.mosk@gmail.com>
2023-09-16 09:33:16 +0200

    create Hilt graph

---
+++ app/src/main/java/io/dnatechnology/dnataskandroid/ui/DnaTaskApplication.kt
@@ -0,0 +1,8 @@
+package io.dnatechnology.dnataskandroid.ui
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DnaTaskApplication : Application() {
+}
